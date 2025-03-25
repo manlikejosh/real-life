7de26cbbcf3a09ede59267df2fb04028294be70d
Joshua Hodgson <114022342+manlikejosh@users.noreply.github.com>
2025-03-25 13:21:48 -0500

    inital commit

---
+++ real-life/real-life/real_lifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  real_lifeApp.swift
+//  real-life
+//
+//  Created by Joshua Hodgson on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct real_lifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ real-life/real-lifeTests/real_lifeTests.swift
@@ -0,0 +1,17 @@
+//
+//  real_lifeTests.swift
+//  real-lifeTests
+//
+//  Created by Joshua Hodgson on 3/25/25.
+//
+
+import Testing
+@testable import real_life
+
+struct real_lifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
